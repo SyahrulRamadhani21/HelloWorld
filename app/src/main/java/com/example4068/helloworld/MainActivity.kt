f986fe8f6ef436414d7f3dec4506b429afa5b73d
syahrul <ababilazies@gmail.com>
2020-02-16 16:11:42 +0700

    Rotate

--- app/src/main/java/com/example4068/helloworld/MainActivity.kt
+++ app/src/main/java/com/example4068/helloworld/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example4068.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState !=null){
+            textView.text=savedInstanceState.getString("text")
+        }
+    }
+
+    fun HelloAndroid(view: View) {
+        textView.text="Hello Android"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString("text",textView.text.toString())
+        super.onSaveInstanceState(outState)
     }
 }
